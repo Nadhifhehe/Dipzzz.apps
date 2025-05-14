810045803f3b45a38715c787fb4d706b4fb1f00c
Nadhifhehe <68574050+Nadhifhehe@users.noreply.github.com>
2025-05-14 15:14:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+col1, col2, col3 = st.columns(3)
+
+with col1
+  st.header("kucing")
+  st.image("https://static.streamlit.io/examples/cat.jpg")
